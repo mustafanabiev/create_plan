5152782244a107b7edbbe4cedce0465c231d012a
AckmatoffITC <106301056+AckmatoffITC@users.noreply.github.com>
2023-01-25 14:12:28 +0600

    create plan app formated

---
+++ lib/components/input/pass_form_field/cubit/cubit/check_box_cubit.dart
@@ -0,0 +1,12 @@
+// import 'package:bloc/bloc.dart';
+// import 'package:equatable/equatable.dart';
+
+// part 'check_box_state.dart';
+
+// class CheckBoxCubit extends Cubit<CheckBoxState> {
+//   CheckBoxCubit() : super(CheckBoxInitial());
+
+//   void change(bool isChecked) {
+//     emit(!isChecked);
+//   }
+// }
