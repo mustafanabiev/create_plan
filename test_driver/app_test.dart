6379a7fa2c111172576b5211027c8555ef5dcd65
AckmatoffITC <106301056+AckmatoffITC@users.noreply.github.com>
2023-02-07 12:53:13 +0600

    integration test

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,14 +1,28 @@
 import 'package:flutter_driver/flutter_driver.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
+
+import 'helpers/helpers.dart';
 
 void main() {
   late FlutterDriver driver;
 
-  group('CreatePlan App', () {
+  group('Create Plan Integration Test', () {
     setUpAll(() async {
       driver = await FlutterDriver.connect();
 
       await driver.waitUntilFirstFrameRasterized();
     });
+  });
+
+  test('login-select-lang-view', () async {
+    await driver.waitFor(find.byValueKey('login-select-view'));
+
+    await takeScreenshot(driver, 'login-select-view');
+    await driver.tap(find.byValueKey('login-button'));
+    await Future<void>.delayed(const Duration(seconds: 5));
+  });
+
+  tearDownAll(() async {
+    await driver.close();
   });
 }
